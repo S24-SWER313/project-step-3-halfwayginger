50603d3ef7abc12cf1ba20d025a0fbb5d9d71e6a
ObadaTah <obadatahboub20@gmail.com>
2024-06-07 10:06:26 +0300

    post-service

---
+++ post-service/src/main/java/edu/bethlehem/post_service/Error/GeneralException.java
@@ -0,0 +1,24 @@
+package edu.bethlehem.post_service.Error;
+
+import org.springframework.http.HttpStatus;
+
+public class GeneralException extends RuntimeException {
+
+    private HttpStatus httpStatus;
+
+    public GeneralException(String message, HttpStatus httpStatus) {
+        super(message);
+
+        this.httpStatus = httpStatus;
+    }
+
+    public GeneralException(String message) {
+
+        super(message);
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+}
