0d7df99fa2ccf43d1719d19768a876d9b757150b
ObadaTah <obadatahboub20@gmail.com>
2024-06-07 10:07:47 +0300

    feat: Add PostServiceApplicationTests for testing the post-service

---
+++ post-service/src/test/java/edu/bethlehem/post_service/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bethlehem.post_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
